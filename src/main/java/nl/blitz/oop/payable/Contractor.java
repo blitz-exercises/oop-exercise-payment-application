4782df33c7f9ea82fe8e0157d0f188ae960d268e
Mikev <mikyboy686@gmail.com>
2025-11-04 14:50:18 +0100

    payment application implementation

--- src/main/java/nl/blitz/oop/payable/Contractor.java
+++ src/main/java/nl/blitz/oop/payable/Contractor.java
@@ -2,34 +2,52 @@ package nl.blitz.oop.payable;
 
 public class Contractor implements Payable {
 
-    private String name;
+    private final String name;
     private double dayRate;
     private double approvedDays;
 
     public Contractor(String name, double dayRate, double approvedDays) {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        if (name.isBlank() || name == null) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (dayRate <= 0) {
+            throw new IllegalArgumentException("dayrate must be positive");
+        }
+        if (approvedDays < 0) {
+            throw new IllegalArgumentException("approved days cannot be negative");
+        }
+
+        this.name = name;
+        this.dayRate = dayRate;
+        this.approvedDays = approvedDays;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.name;
     }
 
     public double getDayRate() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.dayRate;
     }
 
     public double getApprovedDays() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.approvedDays;
     }
 
     @Override
     public double calculatePay() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return (this.getDayRate() * this.getApprovedDays());
     }
 
     @Override
     public String printLine() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return "Contractor: " + this.getName() + " Pay is: " + this.calculatePay();
     }
 }
 
--- src/main/java/nl/blitz/oop/payable/InvoiceItem.java
+++ src/main/java/nl/blitz/oop/payable/InvoiceItem.java
@@ -7,29 +7,47 @@ public class InvoiceItem implements Payable {
     private int quantity;
 
     public InvoiceItem(String description, double unitPrice, int quantity) {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        if (description.isBlank() || description == null) {
+            throw new IllegalArgumentException("Description cannot be empty");
+        }
+        if (unitPrice <= 0) {
+            throw new IllegalArgumentException("unit price must be greater than 0");
+        }
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative or 0");
+        }
+
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
     }
 
     public String getDescription() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.description;
     }
 
     public double getUnitPrice() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.unitPrice;
     }
 
     public int getQuantity() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.quantity;
     }
 
     @Override
     public double calculatePay() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return (this.getUnitPrice() * this.getQuantity());
     }
 
     @Override
     public String printLine() {
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
+        return this.getDescription() + ": " + this.getQuantity() + " * " + this.getUnitPrice() + " = " + this.calculatePay(); 
     }
 }
 
